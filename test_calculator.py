30146a874613e54b7784e01d72403459c0cfa03b
Surya Ramesh <emailsuryaramesh@gmail.com>
2025-11-19 17:36:25 -0500

    m

--- test_calculator.py
+++ test_calculator.py
@@ -32,7 +32,7 @@ class TestCalculator(unittest.TestCase):
     ####### Partner 2
     def test_divide_by_zero(self): # 1 assertion
         with self.assertRaises(ZeroDivisionError):
-            div(0, 5)
+            div(5, 0)
 
 
     def test_logarithm(self): # 3 assertions
